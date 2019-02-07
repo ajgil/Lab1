dffe9075a728a5700267f140536759b13696ac97
Toño <agilpalomino@costaisa.com>
2019-02-07 11:46:23 +0100

    first comit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// Decorador -- se el pasan los objetos selectos, template y estilos.
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+})
+
+export class HeaderComponent {
+
+}

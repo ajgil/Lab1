565c54d99663905e08dceafae8d031d7e59d03b7
Toño <agilpalomino@costaisa.com>
2019-02-08 10:53:07 +0100

    updates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class recipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Receta Dummy','Esta receta es para dummies',
+     'https://upload.wikimedia.org/wikipedia/commons/2/27/Brocheta-frutas-recetas-faciles-ninos.jpg'),
+     new Recipe('Receta Dummy','Receta para dummies 2',
+     'https://upload.wikimedia.org/wikipedia/commons/2/27/Brocheta-frutas-recetas-faciles-ninos.jpg'),
+  ];
   constructor() { }
 
   ngOnInit() {

2b7a8081c79c444daec8b0f07e0533c56163c703
Toñin <tonin@Tonin.local>
2019-02-17 21:21:05 +0100

    shopping list updated

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,22 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
 export class ShoppingListService {
 
+    // Creamos un evento para informar al resto que hay nuevos ingredientes
+    // Emite el array completo
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient('Manzanas', 5),
+        new Ingredient('Tomates', 10),
+      ];
+    
+    getIngredients(){
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
